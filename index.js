4e9d3306a3d42bd1e2a87f3c9e767330139f35c3
Wouter Scherphof <wouter.scherphof@gmail.com>
2022-01-20 17:06:55 +0100

    new linter

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict";
 
-const fp = require('fastify-plugin')
+const fp = require("fastify-plugin");
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 
-const crypto = require('crypto')
-const { promisify } = require('util')
-const scrypt = promisify(crypto.scrypt)
-const randomBytes = promisify(crypto.randomBytes)
-const { timingSafeEqual } = crypto
-const LEN = 32
+const crypto = require("crypto");
+const { promisify } = require("util");
+const scrypt = promisify(crypto.scrypt);
+const randomBytes = promisify(crypto.randomBytes);
+const { timingSafeEqual } = crypto;
+const LEN = 32;
 
 // nicked from https://github.com/patrickpissurno/fastify-esso
 /**
@@ -19,91 +19,86 @@ const LEN = 32
  * @param { string } data
  * @returns { Promise<string> }
  */
-async function encrypt (key, data) {
-  data = JSON.stringify(data)
-  const iv = await randomBytes(16)
-  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv)
+async function encrypt(key, data) {
+  data = JSON.stringify(data);
+  const iv = await randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
   return new Promise((resolve, reject) => {
-    let encrypted = ''
-    cipher.on('readable', () => {
-      let chunk
+    let encrypted = "";
+    cipher.on("readable", () => {
+      let chunk;
       while ((chunk = cipher.read()) !== null) {
-        encrypted += chunk.toString('hex')
+        encrypted += chunk.toString("hex");
       }
-    })
-    cipher.on('end', () => {
-      resolve([iv.toString('hex'), encrypted].join(':'))
-    })
-    cipher.on('error', reject)
+    });
+    cipher.on("end", () => {
+      resolve([iv.toString("hex"), encrypted].join(":"));
+    });
+    cipher.on("error", reject);
 
-    cipher.write(data)
-    cipher.end()
-  })
+    cipher.write(data);
+    cipher.end();
+  });
 }
 
 // nicked from https://github.com/patrickpissurno/fastify-esso
 /**
-* Turns ciphertext into cleartext
-* @param { Buffer } key
-* @param { string } data
-* @returns { Promise<string> }
-*/
-async function decrypt (key, data) {
-  const [iv, encrypted] = data.split(':')
+ * Turns ciphertext into cleartext
+ * @param { Buffer } key
+ * @param { string } data
+ * @returns { Promise<string> }
+ */
+async function decrypt(key, data) {
+  const [iv, encrypted] = data.split(":");
   const decipher = crypto.createDecipheriv(
-    'aes-256-cbc',
+    "aes-256-cbc",
     key,
-    Buffer.from(iv, 'hex')
-  )
+    Buffer.from(iv, "hex")
+  );
   return new Promise((resolve, reject) => {
-    let chunk
-    let decrypted = ''
-    decipher.on('readable', () => {
+    let chunk;
+    let decrypted = "";
+    decipher.on("readable", () => {
       while ((chunk = decipher.read()) !== null) {
-        decrypted += chunk.toString('utf8')
+        decrypted += chunk.toString("utf8");
       }
-    })
-    decipher.on('end', () => resolve(
-      JSON.parse(decrypted)
-    ))
-    decipher.on('error', reject)
+    });
+    decipher.on("end", () => resolve(JSON.parse(decrypted)));
+    decipher.on("error", reject);
 
-    decipher.write(encrypted, 'hex')
-    decipher.end()
-  })
+    decipher.write(encrypted, "hex");
+    decipher.end();
+  });
 }
-async function random () {
-  return randomBytes(LEN)
+async function random() {
+  return randomBytes(LEN);
 }
 
-async function plugin (fastify, options) {
+async function plugin(fastify, options) {
   // function returning a Promise
-  const { key } = options
+  const { key } = options;
 
-  fastify.decorate('crypto', {
-    async generateKey () {
+  fastify.decorate("crypto", {
+    async generateKey() {
       // Buffer
-      return scrypt(await random(), await random(), LEN)
+      return scrypt(await random(), await random(), LEN);
     },
-    async encrypt (data) {
-      return encrypt(await key(), data)
+    async encrypt(data) {
+      return encrypt(await key(), data);
     },
-    async decrypt (data) {
-      return decrypt(await key(), data)
+    async decrypt(data) {
+      return decrypt(await key(), data);
     },
-    async hash (password) {
-      const salt = await random()
-      const derivedKey = await scrypt(password, salt, LEN)
+    async hash(password) {
+      const salt = await random();
+      const derivedKey = await scrypt(password, salt, LEN);
       // 2 x Buffer
-      return { salt, derivedKey }
+      return { salt, derivedKey };
     },
-    async verify ({ salt, derivedKey }, password) {
-      return timingSafeEqual(
-        await scrypt(password, salt, LEN),
-        derivedKey
-      )
-    }
-  })
+    async verify({ salt, derivedKey }, password) {
+      return timingSafeEqual(await scrypt(password, salt, LEN), derivedKey);
+    },
+  });
 }
 
-module.exports = fp(plugin, { name: 'fastify-crypto' })
+module.exports = fp(plugin, { name: "fastify-crypto" });
